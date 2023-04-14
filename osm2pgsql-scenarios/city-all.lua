70576ec7ad7f53c902ce7aa8685b25a2c8191ef3
Josh <jdcarls2@gmail.com>
2023-04-13 22:35:56 -0500

    finish osm2pgsql config

--- osm2pgsql-scenarios/city-all.lua
+++ osm2pgsql-scenarios/city-all.lua
@@ -1,20 +1,71 @@
 local nodes = osm2pgsql.define_table({
     name = 'osm2pgsql_nodes',
-    ids = { type = 'any', type_column = 'osm_type', id_column = 'osm_id' },
+    ids = { type = 'any', id_column = 'osm_id' },
     columns = {
-        { column = 'tags', type = 'hstore'},
-        { column = 'the_geom', type = 'point', not_null = true },
-}})
-
-function process_node(object, geom)
-    local a = {
-        geom = geom,
-        tags = object.tags
+        { column = 'id', sql_type = 'serial', create_only = true },
+        { column = 'tags', type = 'hstore' },
+        { column = 'the_geom', type = 'point', not_null = true }
     }
+})
 
-    nodes:insert(a)
-end
+local ways = osm2pgsql.define_table({
+    name = 'osm2pgsql_ways',
+    ids = { type = 'way', id_column = 'osm_id' },
+    columns = {
+        { column = 'id', sql_type = 'serial', create_only = true },
+        { column = 'tags', type = 'hstore' },
+        { column = 'the_geom', type = 'multilinestring', not_null = true}
+    }
+})
+
+local areas = osm2pgsql.define_table({
+    name = 'osm2pgsql_areas',
+    ids = { type = 'any', id_column = 'osm_id' },
+    columns = {
+        { column = 'id', sql_type = 'serial', create_only = true },
+        { column = 'tags', type = 'hstore' },
+        { column = 'the_geom', type = 'multipolygon', not_null = true}
+    }
+})
 
 function osm2pgsql.process_node(object)
-    process_node(object, object:as_point())
+    nodes:insert({
+        tags = object.tags,
+        the_geom = object:as_point()
+    })
+end
+
+function osm2pgsql.process_way(object)
+    if object.is_closed then
+        areas:insert({
+            tags = object.tags,
+            the_geom = object:as_polygon()
+        })
+        
+        -- get centroid of any closed way
+        nodes:insert({
+            tags = object.tags,
+            the_geom = object:as_polygon():centroid()
+        })
+    end
+
+    ways:insert({
+        tags = object.tags,
+        the_geom = object:as_linestring()
+    })
+end
+
+
+function osm2pgsql.process_relation(object)
+    ways:insert({
+        tags = object.tags,
+        the_geom = object:as_multilinestring():line_merge()
+    })
+
+    if object.tags.type == 'multipolygon' or object.tags.type == 'boundary' then
+        areas:insert({
+            tags = object.tags,
+            the_geom = object:as_multipolygon()
+        })
+    end
 end
