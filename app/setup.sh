709a121dda10b099f5aa771535d34d62ca55cac6
Josh <jdcarls2@gmail.com>
2023-05-03 21:30:21 -0500

    remove extra whitespace

--- app/setup.sh
+++ app/setup.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-
 # set up directories
 mkdir -p data/cache
-
 # get latest geofabric extract of IL
 cd data
 wget -N https://download.geofabrik.de/north-america/us/illinois-latest.osm.pbf
-
